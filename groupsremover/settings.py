1ad62a2a1304f43e9ed462776ee5374ecd07711b
SamPan96 <samuelpanzieri@yahoo.it>
2022-06-09 19:51:53 +0300

    /

--- groupsremover/settings.py
+++ groupsremover/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
